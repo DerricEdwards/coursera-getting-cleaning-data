b6d9244ef07297c27e1885d14c93c9bd1ee82b68
DerricEdwards <derricedwardsedu@gmail.com>
2014-10-26 15:46:34 -0700

    Added readme and script
    See title

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ coursera-getting-cleaning-data
 ==============================
 
 Repository for the Getting and Cleaning Data Course Project
+
+## Setup Steps
+
+* Unzip the source files (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) into a folder on your desired drive.
+* Place the run_analysis.R script into that same unzipped folder ("C:/.../UCI HAR Dataset")
+* Make sure the set the working directory of RStudio to be the same as the unzipped folder. To do this, use the following command: setwd("/C:/.../UCI HAR Dataset"). Make sure to replace the "..." with the approriate path on your disk.
+* Use varName <- read.table("data_set_with_averages.txt") to read in the averaged and tidied data. It has 30 different subjects and 6 activities. If you want to, you can use varName <- read.table("merged_clean_data.txt") to read the merged and clean data table.
+
+## How the Script Works
+
+run_analysis.R performs the following tasks in order to clean up the data set in a tidy fashion.
+
+* Merge the training and test sets into one data set. Specficially, the merge order is train/X_train.txt with test/X_test.txt.
+* Read features.txt and extract only the measurements on the mean and standard deviation for each measurement.
+* Read in the activity_labels.txt file, and use the six labels present for the descriptive activity names. Use these names to name the activities in the merged data set.
+* Label the data set with descriptive activity names. This time, make sure that all the names are converted to lower case, and extraneous characters such as parentheses and underscores are removed. Then merge all the three major data frames together, these being: the one containing the list of features (X), the one containing the activity labels (Y), and the one containing the subject IDs (S). Write this merged frame onto the disk with the name merged_clean_data.txt.
+* Create a independent second tidy data set using the average of each measurement for each activity and subject. The data frame is structured as follows: first column contains the subject IDs, second column contains the activity names, and third column contains the averages for each of the attributes. This new data frame is stored on disk as data_set_with_averages.txt
---
+++ run_analysis.R
@@ -0,0 +1,75 @@
+# Data source needed for this project is located at:
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+# Make sure the working directory is set to be inside of that folder!
+
+# This R script performs the following tasks in this order:
+
+# 1. Merges the training and the test sets to create one data set.
+# It does so by loading those sets up and rowbinding them together into three individual data sets:
+# X, S, and Y.
+
+train <- read.table("train/X_train.txt")
+test <- read.table("test/X_test.txt")
+X <- rbind(train, test)
+
+train <- read.table("train/subject_train.txt")
+test <- read.table("test/subject_test.txt")
+S <- rbind(train, test)
+
+train <- read.table("train/y_train.txt")
+test <- read.table("test/y_test.txt")
+Y <- rbind(train, test)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# It does so by reading in the features file, and using a regex and grep in order to extract
+# the indices of the desired features, and then setting the names of the X table to the appropriate
+# values.
+features <- read.table("features.txt")
+good_features_indexes <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+X <- X[, good_features_indexes]
+
+names(X) <- features[good_features_indexes, 2]
+names(X) <- gsub("\\(|\\)", "", names(X))
+names(X) <- tolower(names(X))
+
+# 3. Uses descriptive activity names to name the activities in the data set.
+# Perform a substitution on the inputed activity labels, and place them in the Y table.
+activities <- read.table("activity_labels.txt")
+activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
+Y[, 1] = activities[Y[, 1], 2]
+names(Y) <- "activity"
+
+# 4. Appropriately labels the data set with descriptive activity names.
+# Apply the descriptive activity names from the earlier steps, by column binding all three tables.
+# Then write the merged table into a file.
+names(S) <- "subject"
+cleaned <- cbind(S, Y, X)
+write.table(cleaned, "merged_clean_data.txt")
+
+# 5. From the data set in step 4, Creates a 2nd, independent tidy data set with the average 
+# of each variable for each activity and each subject.
+# Collect the unique subjects, and use them to to determine the number of subjects. Then determine the number
+# of activities using the length of the activities table.
+uniqueSubjects = unique(S)[, 1]
+numSubjects = length(unique(S)[, 1])
+numActivities = length(activities[, 1])
+numCols = dim(cleaned)[2]
+result = cleaned[1:(numSubjects*numActivities), ]
+row = 1
+
+# Now, put together the tidy data set with the average of each varaible for each activity and subject.
+for (s in 1:numSubjects) 
+{
+  for (a in 1:numActivities) 
+  {
+    result[row, 1] = uniqueSubjects[s]
+    result[row, 2] = activities[a, 2]
+    tmp <- cleaned[cleaned$subject == s & cleaned$activity == activities[a, 2], ]
+    result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
+    row = row + 1
+  }
+}
+
+# Write the tidy data set to a file.
+write.table(result, "data_set_with_averages.txt", row.name = FALSE)
